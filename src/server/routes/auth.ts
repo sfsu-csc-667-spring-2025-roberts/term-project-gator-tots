e8e37426badce02c636a69e9eb14db44b07793a9
John Bagwell <johnbagwell89@gmail.com>
2025-04-16 21:23:06 -0700

    feat: implemented barebones registration frontend

--- src/server/routes/auth.ts
+++ src/server/routes/auth.ts
@@ -4,7 +4,11 @@ import { Request, Response } from "express";
 const router = express.Router();
 
 router.get("/register", async (_request: Request, response: Response) => {
-  response.send("Register");
+  response.render("auth/register");
+});
+
+router.post("/register", async (request: Request, response: Response) => {
+  const { username, password } = request.body;
 });
 
 router.get("/login", async (_request: Request, response: Response) => {
